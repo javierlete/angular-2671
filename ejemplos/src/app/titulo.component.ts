76c0b6dfffeeb50c6634d9aafc7c4c83141b3c7a
Javier <javier@eskura21.com>
2024-04-16 20:28:22 +0200

    Título personalizable por variable texto

--- ejemplos/src/app/titulo.component.ts
+++ ejemplos/src/app/titulo.component.ts
@@ -2,9 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'titulo',
-    template: '<h1>Título</h1>',
-    standalone: true
+    template: '<h1>{{texto}}</h1>',
+    standalone: true,
 })
 export class TituloComponent {
-
+    texto = 'Título personalizado';
 }
