467ee00d9fbd4b9653de61585b8131b7a63e189d
Javier <javier@eskura21.com>
2024-04-16 20:18:48 +0200

    TituloComponent

--- ejemplos/src/app/app.component.ts
+++ ejemplos/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TituloComponent } from './titulo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TituloComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ ejemplos/src/app/titulo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'titulo',
+    template: '<h1>Título</h1>',
+    standalone: true
+})
+export class TituloComponent {
+
+}
