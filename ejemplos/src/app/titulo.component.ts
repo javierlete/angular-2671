6ef35cabbca9499b2164c894b7f1fb2c9ad73087
Javier <javier@eskura21.com>
2024-04-16 20:42:26 +0200

    Titulo personalizable a través de atributo

--- ejemplos/src/app/titulo.component.ts
+++ ejemplos/src/app/titulo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'titulo',
@@ -6,5 +6,5 @@ import { Component } from "@angular/core";
     standalone: true,
 })
 export class TituloComponent {
-    texto = 'Título personalizado';
+    @Input() texto = 'Título personalizado';
 }
