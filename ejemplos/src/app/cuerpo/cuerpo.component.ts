6a2f32c6bf91bb47461d6d68e37afc6496b76969
Javier <javier@eskura21.com>
2024-04-17 19:10:25 +0200

    ng g c cuerpo

---
+++ ejemplos/src/app/cuerpo/cuerpo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cuerpo',
+  standalone: true,
+  imports: [],
+  templateUrl: './cuerpo.component.html',
+  styleUrl: './cuerpo.component.css'
+})
+export class CuerpoComponent {
+
+}
