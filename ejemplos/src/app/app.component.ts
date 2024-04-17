00eb317d3fa9788f3411e83fbeeff54b1fcfceeb
Javier <javier@eskura21.com>
2024-04-17 19:08:16 +0200

    Incluimos el pie en el app-root

--- ejemplos/src/app/app.component.ts
+++ ejemplos/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { TituloComponent } from './titulo.component';
 import { LabelInputComponent } from './labelinput.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
+import { PieComponent } from './pie/pie.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TituloComponent, LabelInputComponent, CabeceraComponent],
+  imports: [RouterOutlet, TituloComponent, LabelInputComponent, CabeceraComponent, PieComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
