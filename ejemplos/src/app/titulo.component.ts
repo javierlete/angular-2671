ecc23030eb3348683720bbefe4db3f3d20fd91c6
Javier <javier@eskura21.com>
2024-04-17 20:16:38 +0200

    Estilo para el título

--- ejemplos/src/app/titulo.component.ts
+++ ejemplos/src/app/titulo.component.ts
@@ -3,6 +3,14 @@ import { Component, Input } from "@angular/core";
 @Component({
     selector: 'titulo',
     template: '<h1>{{texto}}</h1>',
+    styles: `
+        h1 {
+            margin-top: 0;
+            background-color: black;
+            color: white;
+            padding: 0.25rem;
+            text-align: center;
+        }`,
     standalone: true,
 })
 export class TituloComponent {
