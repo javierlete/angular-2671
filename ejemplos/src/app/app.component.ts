9660e3824e91efc6bd369ddde90f4ec723b64c1b
Javier <javier@eskura21.com>
2024-04-17 19:04:36 +0200

    Incluir el componente en el app-root

--- ejemplos/src/app/app.component.ts
+++ ejemplos/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TituloComponent } from './titulo.component';
 import { LabelInputComponent } from './labelinput.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TituloComponent, LabelInputComponent],
+  imports: [RouterOutlet, TituloComponent, LabelInputComponent, CabeceraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
