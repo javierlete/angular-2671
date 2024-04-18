de6ad741ccc6a5b56d6a1569ff382c6511fd37ca
Javier <javier@eskura21.com>
2024-04-18 20:51:24 +0200

    Reset para el contador

--- ejemplos/src/app/contador.component.ts
+++ ejemplos/src/app/contador.component.ts
@@ -2,15 +2,28 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
     selector: 'contador',
-    template: '<button (click)="onBotonPulsado()">{{numero}}</button>',
+    template: `
+        <button (click)="onBotonPulsado()">{{numero}}</button>
+        <button (click)="onResetearPulsado()">Resetear</button>
+    `,
     standalone: true,
 })
 export class ContadorComponent {
     @Input('inicial') numero = 0;
     @Output() cambioNumero = new EventEmitter<number>();
 
+    onResetearPulsado() {
+        this.numero = 0;
+        this.cambioNumero.emit(this.numero);
+    }
+
     onBotonPulsado() {
         this.numero++;
+
+        if(this.numero > 10) {
+            this.numero = 0;
+        }
+
         this.cambioNumero.emit(this.numero);
     }
 }
