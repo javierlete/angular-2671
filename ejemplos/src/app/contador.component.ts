69f865aa99e0ef917010acf2eeb0a2d5ee2adf70
Javier <javier@eskura21.com>
2024-04-18 19:14:41 +0200

    Contador

---
+++ ejemplos/src/app/contador.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'contador',
+    template: '<button (click)="onBotonPulsado()">{{numero}}</button>',
+    standalone: true,
+})
+export class ContadorComponent {
+    numero = 0;
+
+    onBotonPulsado() {
+        this.numero++;
+    }
+}
--- ejemplos/src/app/cuerpo/cuerpo.component.ts
+++ ejemplos/src/app/cuerpo/cuerpo.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LabelInputComponent } from '../labelinput.component';
+import { ContadorComponent } from '../contador.component';
 
 @Component({
   selector: 'app-cuerpo',
   standalone: true,
-  imports: [LabelInputComponent],
+  imports: [LabelInputComponent, ContadorComponent],
   templateUrl: './cuerpo.component.html',
   styleUrl: './cuerpo.component.css'
 })
