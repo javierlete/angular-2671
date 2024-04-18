4eaed37dfa0bc742c33ae9aeef718e033eff1af1
Javier <javier@eskura21.com>
2024-04-18 18:57:57 +0200

    Notificamos a la cabecera que necesita menu

--- ejemplos/src/app/cabecera/cabecera.component.ts
+++ ejemplos/src/app/cabecera/cabecera.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TituloComponent } from '../titulo.component';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-cabecera',
   standalone: true,
-  imports: [TituloComponent],
+  imports: [TituloComponent, MenuComponent],
   templateUrl: './cabecera.component.html',
   styleUrl: './cabecera.component.css'
 })
