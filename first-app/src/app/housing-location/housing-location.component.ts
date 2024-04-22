3fb1ecea29ea4892a36cfda4f08a16f4e15437cc
Javier <javier@eskura21.com>
2024-04-22 20:10:54 +0200

    First-app: interpolación de plantilla

--- first-app/src/app/housing-location/housing-location.component.ts
+++ first-app/src/app/housing-location/housing-location.component.ts
@@ -6,9 +6,11 @@ import { HousingLocation } from '../housinglocation';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      housing-location works!
-    </p>
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+  </section>
   `,
   styleUrl: './housing-location.component.css'
 })
