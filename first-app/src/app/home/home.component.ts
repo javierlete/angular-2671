106038ce038fe1790d551619d1ef2bdaf21fbac3
Javier <javier@eskura21.com>
2024-04-22 19:27:21 +0200

    First-app: HomeComponent

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -5,10 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      home works!
-    </p>
-  `,
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+`,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
