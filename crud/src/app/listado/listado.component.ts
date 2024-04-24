b6924ad5fa1f5dcec4e988a0ba874db6bd5807bc
Javier <javier@eskura21.com>
2024-04-24 20:14:02 +0200

    Listado con modelo de productos

--- crud/src/app/listado/listado.component.ts
+++ crud/src/app/listado/listado.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Producto } from '../producto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listado',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './listado.component.html',
   styleUrl: './listado.component.css'
 })
 export class ListadoComponent {
-
+  productos: Producto[] = [
+    { id: 1, nombre: 'Monitor', precio: 123.45 },
+    { id: 2, nombre: 'Portátil', precio: 1234.56 },
+    { id: 3, nombre: 'Ratón', precio: 12.34 },
+  ];
 }
