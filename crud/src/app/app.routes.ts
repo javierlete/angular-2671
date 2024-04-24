63801a51ede963d8eb817e0a211ab7f3e8199d0c
Javier <javier@eskura21.com>
2024-04-24 19:44:49 +0200

    Ruta '' y 'formulario'

--- crud/src/app/app.routes.ts
+++ crud/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ListadoComponent } from './listado/listado.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ListadoComponent },
+    { path: 'formulario', component: FormularioComponent },
+];
