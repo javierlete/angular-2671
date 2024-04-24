b2db78f475928ce984ce585c50fae00da993bc06
Javier <javier@eskura21.com>
2024-04-24 19:03:25 +0200

    Mostramos el componente de listado

--- crud/src/app/app.component.ts
+++ crud/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListadoComponent } from './listado/listado.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
