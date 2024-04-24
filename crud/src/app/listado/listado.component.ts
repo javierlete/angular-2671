f22f35ddb14b4c924ded7ed773832e68cdfe1888
Javier <javier@eskura21.com>
2024-04-24 18:59:35 +0200

    ng g c listado

---
+++ crud/src/app/listado/listado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  standalone: true,
+  imports: [],
+  templateUrl: './listado.component.html',
+  styleUrl: './listado.component.css'
+})
+export class ListadoComponent {
+
+}
