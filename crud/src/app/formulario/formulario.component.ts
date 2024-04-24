c88faccf51fc7c2af9cc193d376c179c369a3167
Javier <javier@eskura21.com>
2024-04-24 21:02:49 +0200

    Editar ahora muestra los datos en el formulario

--- crud/src/app/formulario/formulario.component.ts
+++ crud/src/app/formulario/formulario.component.ts
@@ -1,25 +1,32 @@
 import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ProductoService } from '../producto.service';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
   ruta = inject(ActivatedRoute);
   servicio = inject(ProductoService);
-  
+
+  form = new FormGroup({
+    id: new FormControl(0),
+    nombre: new FormControl(''),
+    precio: new FormControl(0),
+  });
+
   constructor() {
     const id = Number(this.ruta.snapshot.params['id']);
-    
-    if(id) {
-      this.servicio.obtenerPorId(id).then(productoRecibido => {
-        console.log(productoRecibido);
-      });
+
+    if (id) {
+      this.servicio.obtenerPorId(id).then(
+        productoRecibido => this.form.setValue(productoRecibido)
+      );
     }
   }
 }
