6867e48192c0f39dabf62969253d75c045d35093
Javier <javier@eskura21.com>
2024-04-24 19:27:09 +0200

    Modificamos app para mostrar el formulario

--- crud/src/app/app.component.ts
+++ crud/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListadoComponent } from './listado/listado.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListadoComponent],
+  imports: [RouterOutlet, ListadoComponent, FormularioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
