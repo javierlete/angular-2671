579964fc395e5dcaafc73dd0883de189d085a238
Javier <javier@eskura21.com>
2024-04-25 19:36:02 +0200

    Añadimos insertar y modificar al servicio

--- crud/src/app/producto.service.ts
+++ crud/src/app/producto.service.ts
@@ -16,7 +16,29 @@ export class ProductoService {
     const respuesta = await fetch(this.url + id);
     return respuesta.json();
   }
-  
+
+  async insertar(producto: Producto): Promise<Producto> {
+    const respuesta = await fetch(this.url, {
+      method: 'POST',
+      body: JSON.stringify(producto),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+    return respuesta.json();
+  }
+
+  async modificar(producto: Producto): Promise<Producto> {
+    const respuesta = await fetch(this.url + producto.id, {
+      method: 'PUT',
+      body: JSON.stringify(producto),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+    return respuesta.json();
+  }
+
   async borrar(id: number): Promise<any> {
     return await fetch(this.url + id, { method: 'DELETE' });
   }
