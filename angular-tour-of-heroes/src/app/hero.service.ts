3a8664435dc153ad82001dd1f0aea444656e4046
Javier <javier@eskura21.com>
2024-04-30 19:14:35 +0200

    tour: control de errores y mensajes

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { Observable, of } from 'rxjs';
+import { Observable, catchError, of, tap } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient } from '@angular/common/http';
 
@@ -15,10 +15,27 @@ export class HeroService {
     private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(_ => this.log('Se han pedido los heroes')),
+      catchError(_ => {
+        this.log('Ha habido un error al pedir los heroes');
+        return of([]); 
+      })
+    );
   }
 
   getHero(id: number): Observable<Hero> {
-    return this.http.get<Hero>(`${this.heroesUrl}/${id}`);
+    return this.http.get<Hero>(`${this.heroesUrl}/${id}`).pipe(
+      tap(_ => this.log('Se han pedido el heroe ' + id)),
+      catchError(_ => {
+        this.log('Ha habido un error al pedir el heroe ' + id);
+        return of({} as Hero); 
+      })
+    );
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
