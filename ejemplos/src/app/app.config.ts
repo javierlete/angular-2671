01a5fdfecd1bdc76f2acd466e3800f697469aa55
Javier <javier@eskura21.com>
2024-05-02 21:12:27 +0200

    ejemplos: configuración de locale española

--- ejemplos/src/app/app.config.ts
+++ ejemplos/src/app/app.config.ts
@@ -2,6 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+registerLocaleData(localeEs);
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
