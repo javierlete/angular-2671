b50878566ab78bffa12925d2f3cf56ee1d65ece3
Javier <javier@eskura21.com>
2024-05-02 21:12:34 +0200

    Ejemplos de pipes

--- ejemplos/src/app/app.routes.ts
+++ ejemplos/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 export const routes: Routes = [
     { path: '', component: CuerpoComponent },
+    { path: 'pipes', component: PipesComponent },
 ];
--- ejemplos/src/app/pipes/pipes.component.ts
+++ ejemplos/src/app/pipes/pipes.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pipes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pipes.component.html',
   styleUrl: './pipes.component.css'
 })
