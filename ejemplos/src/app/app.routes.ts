5bab581f7b15c7b499c1fb99ea9572200fa8873d
Javier <javier@eskura21.com>
2024-05-02 20:53:37 +0200

    ejemplos: añadimos router

--- ejemplos/src/app/app.routes.ts
+++ ejemplos/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: CuerpoComponent },
+];
--- ejemplos/src/app/menu/menu.component.ts
+++ ejemplos/src/app/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
