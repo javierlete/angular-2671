338e0ce2dd0a2d2596ce44a864f81f408b1314e1
Javier <javier@eskura21.com>
2024-05-02 20:53:54 +0200

    ejemplos: ng g c pipes

---
+++ ejemplos/src/app/pipes/pipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  standalone: true,
+  imports: [],
+  templateUrl: './pipes.component.html',
+  styleUrl: './pipes.component.css'
+})
+export class PipesComponent {
+
+}
