dc00e87ef29261f50a4db137fe9c34b63d24a728
Javier Lete <javier@eskura21.com>
2024-05-13 20:57:42 +0200

    Rutado y componente de directivas

--- ejemplos/src/app/app.routes.ts
+++ ejemplos/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CuerpoComponent } from './cuerpo/cuerpo.component';
 import { PipesComponent } from './pipes/pipes.component';
+import { DirectivasComponent } from './directivas/directivas.component';
 
 export const routes: Routes = [
     { path: '', component: CuerpoComponent },
     { path: 'pipes', component: PipesComponent },
+    { path: 'directivas', component: DirectivasComponent },
 ];
---
+++ ejemplos/src/app/directivas/directivas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directivas',
+  standalone: true,
+  imports: [],
+  templateUrl: './directivas.component.html',
+  styleUrl: './directivas.component.css'
+})
+export class DirectivasComponent {
+
+}
